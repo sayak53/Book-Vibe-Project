5ad22c9b107cb479b499d8344c25e5e7ca7746d6
sayak53 <sayakchakraborty928@gmail.com>
2024-11-01 13:01:34 +0530

    home section fully completed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,22 +1,23 @@
-import bookimg from '../../assets/books.jpg'
+import bookimg from "../../assets/books.jpg";
 const Banner = () => {
-    return (
-        <div className="w-[90%] mx-auto bg-gray-300 rounded-lg mt-8 mb-8 p-10">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src={bookimg}
-            className="w-[90%] lg:w-[60%]" />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+  return (
+    <div className="w-[90%] mx-auto bg-gray-300 rounded-lg mt-8 mb-8 p-10">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={bookimg} className="w-[90%] lg:w-[60%]" />
+        <div>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+            Books to freshen up your bookshelf
+          </h1>
+          <p className="py-6">
+            Books are windows to new worlds, offering knowledge, stories, and
+            endless possibilities within their pages. Books are timeless
+            companions, offering wisdom and comfort, no matter the situation.{" "}
+          </p>
+          <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Banner;
